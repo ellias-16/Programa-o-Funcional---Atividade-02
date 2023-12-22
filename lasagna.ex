41678a42474adc24ea1da4af8e67d0274eb8a03e
ellias-16 <154635837+ellias-16@users.noreply.github.com>
2023-12-22 18:07:53 -0300

    Create lasagna.ex

---
+++ lasagna.ex
@@ -0,0 +1,23 @@
+defmodule Lasagna do
+
+  def expected_minutes_in_oven() do
+    40
+  end
+
+  def remaining_minutes_in_oven(x) do
+    expected_minutes_in_oven() - x
+  end
+
+  def preparation_time_in_minutes(layers) do
+    layers * 2
+  end
+
+  def total_time_in_minutes(layers, x) do
+    x + preparation_time_in_minutes(layers)
+  end
+
+  def alarm() do
+    "Ding!"
+  end
+
+end
